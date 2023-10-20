7d1645a00b2812e29a9d84760b0ec963cd0e1274
Mehmed Mert <mmert1988@gmail.com>
2023-10-20 23:53:19 +0200

    Video 166, 167, 169: High score

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,14 +9,35 @@ public class UIManager : MonoBehaviour
     private Text _scoreText;
 
     [SerializeField]
+    private Text _highScoreText;
+
+    [SerializeField]
     private Image _livesImage;
 
     [SerializeField]
     private Sprite[] _liveSprites;
 
+    private int _highScore = 0;
+
+    private void Start()
+    {
+        UpdateHighScore(PlayerPrefs.GetInt("HighScore", 0));
+    }
+
     public void UpdateScore(int score)
     {
         _scoreText.text = "Score: " + score;
+        if (score > _highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            UpdateHighScore(score);
+        }
+    }
+
+    private void UpdateHighScore(int highScore)
+    {
+        _highScore = highScore;
+        _highScoreText.text = "High score: " + _highScore;
     }
 
     public void UpdateLives(int lives)
